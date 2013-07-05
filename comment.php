4a7eeaad49ebcd7aa95c9f149efabbed0d07fdd5
Gangwon Jo <gangwon@gmail.com>
2013-07-05 21:55:17 +0900

    Added comment.php

---
+++ comment.php
@@ -0,0 +1,27 @@
+<?php
+
+$comments = array();
+$num_comments = 0;
+
+$fp = fopen("./comments/data", "r");
+while (($buffer = fgets($fp, 4096)) !== false)
+{
+	$buffer = trim($buffer);
+	if ($buffer !== "")
+	{
+		$comments[] = $buffer;
+		$num_comments++;
+	}
+}
+fclose($fp);
+
+mt_srand((double)microtime() * 1000000);
+$rand = mt_rand(0, $num_comments - 1);
+
+echo "
+	$(document).ready(function(){
+		$('form#CommentWriteForm input[name=content]').val('".$comments[$rand]."');
+	});
+";
+
+?>
